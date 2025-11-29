58dd943aa7f77d40ecc9de48d26e14319028a9f2
zlfaaasyyy <syauqiyahzalfa2006@gmail.com>
2025-11-30 00:37:24 +0800

    REFACTOR: cleanup code

--- app/Models/Divisi.php
+++ app/Models/Divisi.php
@@ -9,29 +9,19 @@ class Divisi extends Model
 {
     use HasFactory;
 
-    // Nama tabel
     protected $table = 'divisis';
 
-    // Kolom yang dapat diisi
     protected $fillable = [
         'nama',
-        'ketua_divisi_id', // Foreign key ke tabel users
+        'ketua_divisi_id',
         'deskripsi',
     ];
 
-    /**
-     * Relasi ke User (Ketua Divisi)
-     * Satu Divisi memiliki SATU Ketua Divisi (Many-to-One).
-     */
     public function ketuaDivisi()
     {
         return $this->belongsTo(User::class, 'ketua_divisi_id');
     }
 
-    /**
-     * Relasi ke User (Anggota Divisi)
-     * Satu Divisi memiliki BANYAK User/Anggota (One-to-Many).
-     */
     public function users()
     {
         return $this->hasMany(User::class, 'divisi_id');
